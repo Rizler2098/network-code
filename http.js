c001f45a9a7156729cd00d93045d18d44488b5d5
Rizler2098 <116823588+Rizler2098@users.noreply.github.com>
2022-12-24 06:55:14 +0000

    Update http.js

--- http.js
+++ http.js
@@ -27,6 +27,7 @@ try{
     
     
 import {access, constats} from 'node:fs';
+import { open, close} from 'node:fs';
 const file = 'package.json';
 
 access(file, constants.F_OK | constants.W_OK, (err) =>{
@@ -46,8 +47,39 @@ access(file, constants.F_OK | constants.W_OK, (err) =>{
         });
      )
 });
-    
 
+     
+var url = require('url');
+var adr = 'http://localhost:8080/default.html?year=2017&month=february';
+var q = url.parse(adr, true);
+var q = url.parse(adr, true);
+//q.host, q.pathname, q.search     
+   
+var qdata = q.query;// returns an object: {year:2017, month: 'february'}
+
+//return html by using http.createServer
+     
+const http = require('http');
+const url =  require('url');
+const fs = require('fs');
+     
+http.createServer(function (req,res){
+ var query = url.parse(req.url, true);
+ var filename = '.' + query.pathname;
+ fs.readFile(filename, functiopn(err,data){
+               if(err){
+                   res.writenHead(404, {'Content-Type': 'text/html'});
+                   return res.end('4040 not found');
+             
+             }
+              res.writenHead(200, {'Content-Type': 'text/html'});
+              return res.end();
+ });
+}).listen(8080);
+     
+     
+     
+     
 
 
 
