2f7410b610ef8bb73ba2c7752a49abc48fb375e0
Rizler2098 <116823588+Rizler2098@users.noreply.github.com>
2022-12-24 07:59:25 +0000

    Update file_route.js

--- file_route.js
+++ file_route.js
@@ -1,9 +1,12 @@
 import fs from 'node:js';
-
+import {access, constants} from 'node:js';
 
 var createFile = function( name, data) {
     //check if inputs are strings
-    
+    if (typeof myVar === 'string' || myVar instanceof String)
+    // it's a string
+    else
+    // it's something else
     //check if exists
     
     //create file if not
